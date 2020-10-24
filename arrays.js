eaef835e652f95170daf26bc73f6c87b81f3c2e3
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-24 03:04:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,1 @@
-var chocolateBars = [snickers, hundred grand, kitkat, skittles];
 
-function addElementToBeginningOfArray( `foo`, 1) {
-
-}
-
-function destructivelyAddElementToBeginningOfArray(`foo`, 1) {
-
-}
