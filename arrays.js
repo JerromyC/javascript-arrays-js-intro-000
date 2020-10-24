9eb039558408f793238d399f55c92d351d616a74
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-24 03:31:02 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,3 +8,12 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, candyString) {
   chocolateBars.unshift(candyString);
   return chocolateBars;
 }
+
+function addElementToEndOfArray(chocolateBars, candyString) {
+  return [...chocolateBars, candyString];
+}
+
+function destructivelyAddElementToEndOfArray(chocolateBars, candyString) {
+  chocolateBars.push(candyString);
+  return chocolateBars;
+}
