961e9ad737f9f853d8771c5ebff9fe55f1c99312
helpful-algorithm-9205 <darth.vaper3111991@gmail.com>
2020-10-24 03:26:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray(array, element) {
-  chocolateBars.unshift("foo");
+function addElementToBeginningOfArray(chocolateBars, candyString) {
+  return [candyString, ...chocolateBars];
 }
